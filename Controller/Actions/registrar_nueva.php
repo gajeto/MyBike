2cb9e0f39bd2e91d1bd9e54c3cc224070f564ef7
Gustavo <gustavojerezt@gmail.com>
2020-12-14 14:34:58 -0500

    Listo agregar bicicletas

---
+++ Controller/Actions/registrar_nueva.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once (__DIR__."/../MDB/BicicletaMDB.php");
+require_once (__DIR__."/../../Model/Entities/Bicicleta.php");
+$home = "..";
+
+
+$errMsg = 'OK';
+
+$id = 8;
+$estado = $_POST['estado'];
+$nombre = $_POST['nombre'];
+$serial = $_POST['serial'];
+$marca = $_POST['marca'];
+$modelo = $_POST['modelo'];
+$color_p = $_POST['color_p'];
+$color_s = $_POST['color_s'];
+$tipo = $_POST['tipo'];
+$valor = $_POST['valor'];
+$info = $_POST['info'];
+
+
+$bike = new Bicicleta($id,$estado,$serial, $nombre,$marca,$modelo,$color_p,$color_s,$tipo,$valor, $info);
+$operacion = insertarBicicleta($bike);
+
+if($operacion != NULL){
+    header("Location: ../../View/buscar_bicicletas.php"); // ENVIAR AL HOMEPAGES DEL USUARIO
+}else{
+    $errMsg .= 'Usuario y/o contraseña no válido';
+    header("Location: ../../View/index.php"); //ENVIAR AL LOGIN NUEVAMENTE
+}
+
